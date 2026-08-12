850a11d19545519f45a0e0f60aa3065ce468af45
David Alexander <dmalexa5@ncsu.edu>
2026-08-12 16:28:00 -0500

    wip

---
+++ block-app/CHANGELOG.md
@@ -0,0 +1,8 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+
+## [13.2.0](https://github.com/RaspberryPiFoundation/blockly-samples/compare/v13.1.0...v13.2.0) (2026-07-28)
+
+**Note:** Version bump only for package sample-app
---
+++ block-app/src/blocks/text.js
@@ -0,0 +1,57 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as Blockly from 'blockly/core';
+
+// Create a custom block called 'add_text' that adds
+// text to the output div on the sample app.
+// This is just an example and you should replace this with your
+// own custom blocks.
+const addText = {
+  type: 'add_text',
+  message0: 'Add text %1',
+  args0: [
+    {
+      type: 'input_value',
+      name: 'TEXT',
+      check: 'String',
+    },
+  ],
+  previousStatement: null,
+  nextStatement: null,
+  colour: 160,
+  tooltip: '',
+  helpUrl: '',
+};
+
+const setBackgroundColor = {
+  type: 'set_background_color',
+  message0: 'Set background color %1',
+  args0: [
+    {
+      type: 'field_dropdown',
+      name: 'COLOR',
+      options: [
+        ['red', '#f87171'],
+        ['green', '#86efac'],
+        ['blue', '#93c5fd'],
+      ],
+    },
+  ],
+  previousStatement: null,
+  nextStatement: null,
+  colour: 20,
+  tooltip: '',
+  helpUrl: '',
+};
+
+// Create the block definitions for the JSON-only blocks.
+// This does not register their definitions with Blockly.
+// This file has no side effects!
+export const blocks = Blockly.common.createBlockDefinitionsFromJsonArray([
+  addText,
+  setBackgroundColor,
+]);
